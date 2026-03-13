3e69a2b999e1464165c3a8dd26f5cfc2c807efe1
Avish787 <avishbhati2000@gmail.com>
2026-03-13 14:46:30 +0530

    Experiment-4,5 and Assessment-1

---
+++ Index/script.js
@@ -0,0 +1,44 @@
+{console.log("Hello World");
+a=10;
+console.log(a);
+a="Avish";
+console.log(a);}
+console.log(a);
+
+{let name ;
+    name = "Avish";
+    name ="Avish Bhati";
+console.log(name);
+}
+let name="Tony";
+console.log(name);
+const n="Bruce";
+console.log(n);
+
+ let stu = {
+    name:"Avish",
+    age: 22,
+    city: "Delhi"
+ }
+
+ console.log(stu);
+ stu.city = "Moradabad";
+    console.log(stu);
+
+{let a=10;
+let b=20;
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a*b);
+    console.log(a/b);
+    console.log(a%b);
+    console.log(a**b);
+    console.log(a++);
+    console.log(a);
+    console.log(a--);
+    console.log(a);
+}
+
+let x=10;
+x+=5;
+console.log(x);
