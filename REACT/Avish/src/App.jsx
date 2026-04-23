0ebc71dcec31338a487bfde4d90b49206652482e
Avish787 <avishbhati2000@gmail.com>
2026-04-23 12:21:17 +0530

    Update

--- REACT/Avish/src/App.jsx
+++ REACT/Avish/src/App.jsx
@@ -3,6 +3,18 @@ function app(){
     <div className="app">
       <h1>Hello World</h1>
       <p>This is my first react app.</p>
+      <Cse12 />
+    </div>
+  );
+}
+
+//2nd component
+// 2nd Component
+function Cse12(){
+  return (
+    <div className="Cse12">
+      <h2>Welcome to CSE 12</h2>
+      <p>This is some content for the CSE 12 component.</p>
     </div>
   );
 }
